352137047024c02a7caeaa1820b1b403eda87215
Rasmus Odgaard <r.odgaard@gmail.com>
2024-02-14 22:03:42 +0100

    set up big part prefab and changed name from part orbitor

---
+++ Assets/Scripts/BigPart.cs
@@ -0,0 +1,40 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class BigPart : Part
+{
+    [SerializeField] private float orbitTime = 5.0f;
+
+    [SerializeField]
+    Transform plugTransform;
+
+    Sequence Orbit;
+    float SnapDuration = 0.5f;
+
+    float screenMin;
+    float screenMax;
+    float screenBuffer = 1.5f;
+
+    void Start()
+    {
+        screenMin = Camera.main.ViewportToWorldPoint(Vector3.zero).x - screenBuffer;
+        screenMax = Camera.main.ViewportToWorldPoint(Vector3.one).x + screenBuffer;
+
+        transform.position = new Vector3(screenMax + screenBuffer, transform.position.y, transform.position.z);
+
+        Orbit = DOTween.Sequence();
+        Orbit.Append(transform.DOMoveX(screenMin, orbitTime).OnComplete(() => transform.Rotate(0.0f, 0.0f, 180.0f)))
+                .Append(transform.DOMoveX(screenMax, orbitTime).OnComplete(() => transform.Rotate(0.0f, 0.0f, 180.0f))).SetLoops(-1);
+    }
+
+    public override void Snap(Transform socketTransform)
+    {
+        Orbit.Kill();
+        plugTransform.parent = null;
+        plugTransform.parent = socketTransform;
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(plugTransform.DOLocalMove(Vector3.zero, SnapDuration))
+                .Join(plugTransform.DORotateQuaternion(socketTransform.rotation, SnapDuration))
+                .AppendCallback(() => Destroy(gameObject));
+    }
+}
