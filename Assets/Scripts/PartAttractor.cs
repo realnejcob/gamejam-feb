a0aa508beff1276b6bef24980a4403d03164e5ae
Rasmus Odgaard <r.odgaard@gmail.com>
2024-02-15 17:06:23 +0100

    set up snap in all sockets

--- Assets/Scripts/PartAttractor.cs
+++ Assets/Scripts/PartAttractor.cs
@@ -6,52 +6,48 @@ public class PartAttractor : MonoBehaviour
     [SerializeField]
     List<TriggerCollisionHelper> collisionHelpers = new List<TriggerCollisionHelper>();
 
-    private void OnTriggerStay(Collider other)
+    private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-
             for (int i = 0; i < collisionHelpers.Count; i++)
             {
                 TriggerCollisionHelper helper = collisionHelpers[i];
 
-                if (helper.IsTriggered)
+                // Check for trigger collision
+                if (helper.IsTriggered == false)
                 {
-                    Part part;
-                    part = other.transform.root.GetComponent<Part>();
-                    print("Part: " + part.name);
-                    if (part != null)
-                    {
-                        print("Found part");
-                        if (helper.tag == "BigSocket" && part is BigPart)
-                        {
-                            part.Snap(helper.transform);
-                        }
-                        else if (helper.tag == "SmallSocket" && part is SmallPart)
-                        {
-                            part.Snap(helper.transform);
-                        }
-                    }
+                    continue;
+                }
+
+                var other = helper.GetOtherCollider;
+
+                Part part;
+                part = other.transform.root.GetComponent<Part>();
+
+                // Check for part script
+                if (part == null)
+                {
+                    continue;
+                }
+
+                // Check that direction is correct
+                float dot = Vector3.Dot(part.transform.forward, helper.transform.forward);
+                if (dot < 0.8)
+                {
+                    continue;
+                }
+
+                // Check big vs small
+                if (helper.tag == "BigSocket" && part is BigPart)
+                {
+                    part.Snap(helper.transform);
+                }
+                else if (helper.tag == "SmallSocket" && part is SmallPart)
+                {
+                    part.Snap(helper.transform);
                 }
             }
-
-            //print("Closest Transform: " + closestTransform.name);
-
-            //Part part;
-            //part = other.transform.root.GetComponent<Part>();
-            //print("Part: " + part.name);
-            //if (part != null)
-            //{
-            //    print("Found part");
-            //    if (closestTransform.tag == "BigSocket" && part is BigPart)
-            //    {
-            //        part.Snap(closestTransform);
-            //    }
-            //    else if (closestTransform.tag == "SmallSocket" && part is SmallPart)
-            //    {
-            //        part.Snap(closestTransform);
-            //    }
-            //}
         }
     }
 }
