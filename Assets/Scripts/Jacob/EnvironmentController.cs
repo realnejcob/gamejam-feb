040cb2a50909f265bcb19e24a69b710faab913ac
nejcob <jacobhvidamstrup@gmail.com>
2024-02-15 14:16:57 +0100

    PolePlacer placing corners dynamically. Environment can move on WASD.

---
+++ Assets/Scripts/Jacob/EnvironmentController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentController : MonoBehaviour {
+    [SerializeField] private float rotationTime = 0.5f;
+    [SerializeField] private AnimationCurve rotationCurve;
+
+    private bool canTween = true;
+
+    private void Update() {
+        CheckForRotation();
+    }
+
+    private void CheckForRotation() {
+        if (!canTween)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.A)) {
+            DoRotation(Vector3.up);
+        } else if (Input.GetKeyDown(KeyCode.D)) {
+            DoRotation(Vector3.down);
+        }
+    }
+
+    private void DoRotation(Vector3 _axis) {
+        canTween = false;
+        var _currentRotation = gameObject.transform.rotation.eulerAngles;
+        LeanTween.rotateAround(gameObject, _axis, 90, rotationTime)
+            .setEase(rotationCurve)
+            .setOnComplete(() => canTween = true);
+    }
+}
---
+++ Assets/Scripts/PolePlacer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PolePlacer : MonoBehaviour {
+    [SerializeField] private List<Transform> poles = new List<Transform>();
+    private Camera cam;
+
+    private void Awake() {
+        cam = Camera.main;
+        SetupPoles();
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            SetupPoles();
+        }
+    }
+
+    private void SetupPoles() {
+        foreach (var pole in poles) {
+            pole.gameObject.SetActive(true);
+        }
+
+        var leftPos = cam.ScreenToWorldPoint(new Vector3(0, 0));
+        var totalScreenWidthDist = Mathf.Abs(leftPos.x) * 2;
+        poles[0].position = new Vector3(-totalScreenWidthDist / 2, poles[0].position.y, totalScreenWidthDist / 2);
+        poles[1].position = new Vector3(totalScreenWidthDist / 2, poles[1].position.y, totalScreenWidthDist / 2);
+        poles[2].position = new Vector3(-totalScreenWidthDist / 2, poles[2].position.y, -totalScreenWidthDist / 2);
+        poles[3].position = new Vector3(totalScreenWidthDist / 2, poles[3].position.y, -totalScreenWidthDist / 2);
+    }
+}
